afa5acc62c9761ba3c90d17b3e1fe726d3f0dbe1
oyy <oyy0821@chunjae.co.kr>
2023-11-27 09:51:55 +0900

    인라인 스타일을 사용한 동적 스타일링

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,64 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0,
-            name: "",
-            lastName: "",
-            // fullName: ""
-        };
-    },
-    watch: {
-        counter(value) {
-            if(value > 50) {
-                this.counter = 0;
-            }
-        },
-        // name(value) {
-        //     if(value === "") {
-        //         this.fullName = "";
-        //     } else {
-        //         this.fullName = value + " " + this.lastName;
-        //     }
-        // },
-        // lastName(value) {
-        //     if(value === "") {
-        //         this.fullName = "";
-        //     } else {
-        //         this.fullName = this.name + " " + value;
-        //     }
-        // }
-    },
-    methods: {
-        add(num) {
-            this.counter = this.counter + num;
-        },
-        reduce(num) {
-            this.counter = this.counter - num;
-        },
-        setName(event) {
-            // console.log(event.target.value);
-            this.name = event.target.value;
-        },
-        resetInput() {
-            this.name = "";
-        },
-        outputFullName() {
-            console.log("running again!");
-            if(this.name === "") {
-                return "";
-            }
-            return this.name + " " + "Oh";
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false
         }
     },
-    computed: {
-        fullName() {
-            console.log("running again!");
-            if(this.name === "" || this.lastName === "") {
-                return "";
+    methods: {
+        boxSelected(box) {
+            if(box === "A") {
+                this.boxASelected = true;
+            } else if(box === "B") { 
+                this.boxBSelected = true;
+            } else if(box === "C") {
+                this.boxCSelected = true;
             }
-            return this.name + " " + this.lastName;
         }
     }
 });
 
-app.mount('#events');
+app.mount("#styling")
